ccea1be043562188bc9250361014c8da6f79b6c6
Måns Falk <mansfalk@msn.com>
2021-11-19 21:26:28 +0100

    Initial commit

---
+++ constants.go
@@ -0,0 +1,64 @@
+package main
+
+var ranks = [...]Rank{
+	{
+		Name: "Silver",
+		Min:  0,
+		Max:  999,
+	},
+	{
+		Name: "Gold I",
+		Min:  1000,
+		Max:  1099,
+	},
+	{
+		Name: "Gold II",
+		Min:  1100,
+		Max:  1199,
+	},
+	{
+		Name: "Veteran I",
+		Min:  1200,
+		Max:  1299,
+	},
+	{
+		Name: "Veteran II",
+		Min:  1300,
+		Max:  1399,
+	},
+	{
+		Name: "Master I",
+		Min:  1400,
+		Max:  1499,
+	},
+	{
+		Name: "Master II",
+		Min:  1500,
+		Max:  1599,
+	},
+	{
+		Name: "Elite I",
+		Min:  1600,
+		Max:  1699,
+	},
+	{
+		Name: "Elite II",
+		Min:  1700,
+		Max:  1799,
+	},
+	{
+		Name: "Pro I",
+		Min:  1800,
+		Max:  1899,
+	},
+	{
+		Name: "Pro II",
+		Min:  1900,
+		Max:  1999,
+	},
+	{
+		Name: "Legend",
+		Min:  2000,
+		Max:  -1,
+	},
+}
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Rank struct {
+	Name string `json:"name"`
+	Min  int    `json:"min"`
+	Max  int    `json:"max"`
+}
+
+type UserProfile struct {
+	Elo int `json:"elo"`
+}
+
+type GetRankResponse struct {
+	Rank Rank `json:"rank"`
+	Elo  int  `json:"elo"`
+}
+
+func getRankFromElo(elo int) Rank {
+	var rank Rank
+
+	for i := 0; i < len(ranks); i++ {
+		tmpRank := ranks[i]
+		if tmpRank.Min > 0 && elo > tmpRank.Min {
+			if tmpRank.Max > 0 && elo < tmpRank.Max {
+				rank = tmpRank
+				break
+			}
+		}
+	}
+
+	return rank
+}
+
+func getRank(c *gin.Context) {
+	name := c.Param("name")
+
+	resp, err := http.Get(fmt.Sprintf("https://esportal.com/api/user_profile/get?username=%s&rank=1", name))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var profile UserProfile
+	err = json.NewDecoder(resp.Body).Decode(&profile)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	rank := GetRankResponse{
+		Rank: getRankFromElo(profile.Elo),
+		Elo:  profile.Elo,
+	}
+
+	json.NewEncoder(c.Writer).Encode(rank)
+}
+
+func handleRequests() {
+	router := gin.Default()
+	router.GET("/rank/:name", getRank)
+
+	router.Run(":8081")
+}
+
+func main() {
+	handleRequests()
+}
